796034178c8d8c3afc246fb6d265d8ce25120f5f
azure2015 <azuremmii@hotmail.com>
2018-12-31 13:58:47 +0000

    #1 score implemented update
    Score added to pong game. Added more comments.

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@
 
 var bat;    // bat class variable
 var ball;   // ball class variable
+var score = 0;  // Global score variable
 
 function setup() {
   createCanvas(400, 400);  // set up canvas
@@ -18,8 +19,10 @@ function draw() {
   bat.move();
   ball.show();				// display ball and move
   ball.move();
-  ball.check(bat);    // check if ball hits bat
-  
+  if(ball.check(bat)) {     // check if ball hits bat
+  	score += 10;
+  }
+  scoreDisplay();
 }
 
 function keyPressed() {      // keybutton input, window needs focus to work
@@ -32,6 +35,19 @@ function keyPressed() {      // keybutton input, window needs focus to work
   
 }
 
+// Function: Score Display
+// Displays the current score
+
+function scoreDisplay() {
+  textSize(16);
+  fill(40,160,40);
+  text("Score : " + score,20,20);
+}
+
+
+// Class Ball
+// Simple ball square
+
 class Ball {         // ball class even though ball is currently square
   constructor() {
     this.x = 200;
@@ -39,11 +55,12 @@ class Ball {         // ball class even though ball is currently square
     this.dx = -1;    // currently only moving in x plane
     this.dy = 0;
     this.r =  5;    // need to change his
+    this.speed = 3;
   }
   
   move() {
-     this.x += this.dx;   //move ball
-     this.y += this.dy;
+     this.x += (this.dx*this.speed);   //move ball
+     this.y += (this.dy*this.speed);
     
      if(this.x > width-this.r) {  //if ball hits canvas width change direction
        this.dx *= -1; 
@@ -52,25 +69,31 @@ class Ball {         // ball class even though ball is currently square
   
   show() {                   //show ball on screen
     fill(150,50,50);
+    //ellipse(this.x,this.y,this.r*2);
     rect(this.x,this.y,this.r*2,this.r*2);
   }
   
   check(bat) {                      // check if bat and ball collide
-   if(this.x == bat.x+bat.w) {
+   if(this.x >= bat.x-this.r && this.x <= bat.x+this.r) {
        if((this.y >= bat.y-(this.r*2)) && ( this.y  <= bat.y+bat.h+ (this.r*2))) {
            this.dx *= -1;  
+           return true;
+         
      }
    }
   }
 }
 
+
+// Class Bat
+// 
 class Bat {               // Bat Class
 	constructor() {
-   this.x = 20;
+   this.x = 20;            //Starting position
    this.y = height/2;
    this.w = 10
    this.h = 40;
-   this.direction = 0;
+   this.direction = 0;    // start no direction
   }
   
   show() {
